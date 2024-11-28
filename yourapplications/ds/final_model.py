0d87d0e776c98cb3362adc5062e20de2ede8e607
Elen Khanoyan <142159781+el-kh@users.noreply.github.com>
2024-11-28 19:36:36 +0400

    corrected all the errors

---
+++ yourapplications/ds/final_model.py
@@ -0,0 +1,53 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Define your engine (assuming it's already defined in Database.database)
+from yourapplications.etl.Database.database import *
+from yourapplications.etl.Database.models import *
+
+from sqlalchemy import create_engine
+from sqlalchemy import inspect  # Import the SQLAlchemy inspector
+
+def fetch_table_as_dataframe(table_name):
+    """
+    Fetch data from a specific database table and return it as a Pandas DataFrame.
+
+    Args:
+        table_name (str): The name of the table to fetch data from.
+
+    Returns:
+        pd.DataFrame: The table data as a DataFrame.
+    """
+    query = f"SELECT * FROM {table_name}"  # SQL query to select all data
+    with engine.connect() as connection:
+        df = pd.read_sql_query(query, connection)
+        print(f"Fetched {len(df)} rows from table '{table_name}'.")
+        return df
+
+
+customer_df = fetch_table_as_dataframe("customer")
+location_df = fetch_table_as_dataframe("location")
+plan_df = fetch_table_as_dataframe("plan")
+application_df = fetch_table_as_dataframe("application")
+price_df = fetch_table_as_dataframe("price")
+notification_df = fetch_table_as_dataframe("notification")
+subscription_df = fetch_table_as_dataframe("subscription")
+
